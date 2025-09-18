4b5f9dba5f0d1debfa7d6eeb38425867d8f853e5
TomokoKonishi <TomokoKonishi@users.noreply.github.com>
2025-09-18 04:40:09 +0000

    deploy: d1759bad7fabf3c91298f615327c20ecceb821db

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -731,12 +731,6 @@ var store = [{
         "url": "/ocidocs/services/governance%20and%20administration/cost-management/",
         "teaser": "https://files.speakerdeck.com/presentations/4541d035c9b4444bb7b1c2ea5ed032d8/slide_0.jpg"
       },{
-        "title": "コンピュート詳細",
-        "excerpt":"OCI上で仮想マシンやベアメタル・サーバーを利用することができるコンピュート・サービスについての詳細資料です。  カスタム・イメージの応用、仮想マシンイメージの持ち込み(BYOI)、ハイパーバイザーの利用、メトリックと自動スケーリング、コンソール接続、などのコンピュート・サービスの応用的な使い方について解説しています。   スライド          ","categories": ["Services","Compute"],
-        "tags": ["スライドあり","レベル:応用(200)"],
-        "url": "/ocidocs/services/compute/compute-200/",
-        "teaser": "https://files.speakerdeck.com/presentations/90e8e852d1c8443787f6f33e7e721177/slide_0.jpg"
-      },{
         "title": "ロード・バランサ 詳細 - フレキシブル・ロード・バランサ(FLB)",
         "excerpt":"様々なサービスの冗長性やスケーリングをサポートする OCI ロード・バランサー・サービス（FLB）について、応用的な利用方法について解説したドキュメントです。  セッション永続性、仮想ホストによるルーティング、URIパス・ベース・ルーティング、SSL通信方式とロード・バランサーでのSSLターミネーションの設定、ロード・バランサーが収集するメトリック、トラブル時の対応ガイドラインについて解説しています。   スライド            ","categories": ["Services","Networking"],
         "tags": ["スライドあり","レベル:応用(200)"],
@@ -946,4 +940,10 @@ var store = [{
         "tags": ["スライドあり","動画あり"],
         "url": "/ocidocs/updates/update-FY26Q1/",
         "teaser": "https://files.speakerdeck.com/presentations/7a14a26fdd4c400badb48f8d7125f99a/slide_0.jpg"
+      },{
+        "title": "コンピュート詳細",
+        "excerpt":"OCI上で仮想マシンやベアメタル・サーバーを利用することができるコンピュート・サービスについての詳細資料です。   コンピュート・サービスの以下の項目を説明しています。      インスタンスの編集、シェイプ変更   Oracle Cloud Agent   インスタンスの監視（モニタリング・メトリック）   計画メンテナンス（ライブ移行、再起動移行）、障害時のVM再起動、冗長化構成   インスタンスのトラブルシューティング（コンソール履歴の表示、コンソール接続、診断中断の送信、診断リブート）   本資料の範囲外           コンピュート概要と基本機能　→ コンピュート概要 を参照            インスタンス構成、インスタンスプール、自動スケーリング　→ 自動スケーリング機能 概要 を参照       スライド          ","categories": ["Services","Compute"],
+        "tags": ["スライドあり","レベル:応用(200)"],
+        "url": "/ocidocs/services/compute/compute-200/",
+        "teaser": "https://files.speakerdeck.com/presentations/90e8e852d1c8443787f6f33e7e721177/slide_0.jpg"
       }]
